d98a1416102af212896a5833c0415a7bee0e2c90
Antoan Metodiev <Boksnem@abv.bg>
2024-09-24 23:04:10 +0300

    structure data

---
+++ client/src/components/DiscoverPage/structure/utils/allCardsData.js
@@ -0,0 +1,49 @@
+import image1 from "../../resources/images/1.jpg";
+import image2 from "../../resources/images/2.jpg";
+import image3 from "../../resources/images/3.jpg";
+import image4 from "../../resources/images/4.jpg";
+import image5 from "../../resources/images/5.jpg";
+import image6 from "../../resources/images/6.png";
+import image7 from "../../resources/images/7.jpg";
+
+
+const allCardsData = [
+    {
+        image: image1,
+        title: "Hear more of your music.",
+        text: "Enjoy your music like never before on MIDAL. Get the best-in-class lossless audio formats, HiRes FLAC, and Dolby Atmos.",
+    },
+    {
+        image: image2,
+        title: "Over 110 million songs and counting.",
+        text: "Discover your next favorite artist or song in our ad-free music library of over 110 million tracks.",
+    },
+    {
+        image: image5,
+        title: "Curated playlists.",
+        text: "We stay ahead of the next and best in culture. Listen to original playlists from our team or stream guest playlists from your favorite artists.",
+    },
+    {
+        image: image7,
+        title: "MIDAL RISING.",
+        text: "Hear the next wave of MIDAL RISING artists from around the world who are breaking boundaries and genres.",
+    },
+    {
+        image: image4,
+        title: "Live on MIDAL.",
+        text: "Experience music in real-time with friends, family, or fellow fans with Live.",
+    },
+   
+    {
+        image: image3,
+        title: "Mixes and Radio.",
+        text: "Get a personalized playlist with songs tailored to your taste with My Mix, or explore new music with Artist Radio.",
+    },
+    {
+        image: image6,
+        title: "Offline mode.",
+        text: "Listen how you want, on any device, whether you're offline, on the go, at home, or in the car.",
+    },
+];
+
+export default allCardsData;
