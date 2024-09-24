fb424b279df3a32383b660c9048382592c646d9d
Antoan Metodiev <Boksnem@abv.bg>
2024-09-24 17:06:45 +0300

    add and connect MongoDB with mongoose

--- server/index.js
+++ server/index.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // Позволява на Express да обработва JSON тела на заявките
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173", // Първи домейн
+        "http://localhost:5174", // Втори домейн
+        "http://localhost:5175", // Трети домейн
+    ]
 }));
 
+// Свързвам се към база данни MongoDB:
+const mongoURL = 'mongodb://localhost:27017/midal';
+mongoose.connect(mongoURL)
+        .then(() => {
+            console.log('MongoDB is started!!!');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 
 // Слушам когато някой се опита да направи заявка на този URL:
 app.get('/api', (req, res) => {
     res.json({fruits: ["Orange", "Watermelon", "Melon"]});
 });
-
 
 
 const PORT = 8080;
