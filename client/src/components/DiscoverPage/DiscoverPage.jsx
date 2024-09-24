cc8f1f44fd9d6e796ff6af32b9da959fdf87307d
Antoan Metodiev <Boksnem@abv.bg>
2024-09-24 18:37:05 +0300

    create DiscoverPage
    This is the homepage on the application

---
+++ client/src/components/DiscoverPage/DiscoverPage.jsx
@@ -0,0 +1,15 @@
+import Overview from "./structure/Overview/Overview";
+
+const DiscoverPage = () => {
+
+
+
+
+    return (
+        <article>
+            <Overview />
+        </article>
+    );
+};
+
+export default DiscoverPage;
