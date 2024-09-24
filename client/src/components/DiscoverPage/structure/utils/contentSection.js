8869fb4ff260090a3c4419328713e3d8a7b05ffc
Antoan Metodiev <Boksnem@abv.bg>
2024-09-25 01:46:01 +0300

    add file who store data for Sounds Info cards

---
+++ client/src/components/DiscoverPage/structure/utils/contentSection.js
@@ -0,0 +1,20 @@
+const soundsInfoData = [
+    {
+        level: 'Max',
+        upTo: "Up to 24-bit, 192 kHz",
+        text: "Enjoy class-leading audio that reveals every detail with HiRes Free Lossless Audio Codec (HiRes FLAC). For the best experience, use 5G or WiFi with a hardware connection.",
+    },
+    {
+        level: 'High',
+        upTo: "Up to 16-bit, 44.1 kHz",
+        text: "Listen to over 110 million songs in studio quality with FLAC. As an open-source format, any artist can easily create and deliver music in hi-fi quality.",
+    },
+    {
+        level: 'Low',
+        upTo: "Up to 320 kbps",
+        text: "Take the music you love on the go without worrying about data. Useful when you have a weak signal, reached your data limit, or run out of download space.",
+    },
+];
+
+export default soundsInfoData;
+
