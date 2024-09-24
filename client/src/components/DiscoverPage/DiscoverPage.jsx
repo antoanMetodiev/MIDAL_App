9a95c1038603021263e2882125c7a0358b936683
Antoan Metodiev <Boksnem@abv.bg>
2024-09-25 01:46:19 +0300

    add PaymentPlans component

--- client/src/components/DiscoverPage/DiscoverPage.jsx
+++ client/src/components/DiscoverPage/DiscoverPage.jsx
@@ -1,13 +1,17 @@
 import Overview from "./structure/Overview/Overview";
 import CarouselPlaylist from "./structure/CarouselPlaylist/CarouselPlaylist";
+import SoundsInfo from "./structure/SoundsInfo/SoundsInfo";
+import PaymentPlans from "./structure/PaymentPlans/PaymentPlans";
 
 const DiscoverPage = () => {
 
-
+    
     return (
         <article>
             <Overview />
             <CarouselPlaylist />
+            <SoundsInfo />
+            <PaymentPlans />
         </article>
     );
 };
