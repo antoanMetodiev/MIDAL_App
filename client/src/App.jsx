a08f1ac8c0c07a2106907b5c4bfe30af7b83ebac
Antoan Metodiev <Boksnem@abv.bg>
2024-09-27 15:56:47 +0300

    order code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,28 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import axios from "axios"
-import DiscoverPage from './components/DiscoverPage/DiscoverPage'
+import './App.css';
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-const baseURL = '';
+
+import DiscoverPage from './components/DiscoverPage/DiscoverPage';
+import SpotifyAuth from './components/SpotifyAuth/SpotifyAuth';
+import Songs_Podcasts from './components/Songs_Podcasts/Songs_Podcasts';
+import SpotifyHandler from './components/SpotifyAuth/SpotifyHandler';
+import UserAuth from './components/UserAuth/UserAuth';
 
 function App() {
-	const [count, setCount] = useState(0)
+
 
 	return (
 		<>
-			<DiscoverPage />
+			<SpotifyAuth />
+
+			<UserAuth />
+
+			<Routes>
+				<Route path="/" element={<DiscoverPage />}/>
+				<Route path="/songs-and-podcasts" element={<SpotifyHandler />}/>
+				<Route path="/songs-podcasts" element={<Songs_Podcasts />}/>
+			</Routes>
 		</>
 	)
 }
