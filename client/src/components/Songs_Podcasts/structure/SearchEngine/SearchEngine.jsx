524538b1a9df3788cf543015c8f24801d438375d
Antoan Metodiev <Boksnem@abv.bg>
2024-09-27 15:52:31 +0300

    add SearchEngine for songs

---
+++ client/src/components/Songs_Podcasts/structure/SearchEngine/SearchEngine.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import style from "./SearchEngine.module.css";
+import axios from "axios";
+
+const SearchEngine = () => {
+    const [results, setResults] = useState([]);
+
+    async function checkForSong(event) {
+        event.preventDefault();
+        const songName = event.target.song_name.value;
+
+        debugger;
+
+        const token = Cookies.get('spotify_token');
+        const response = await axios.get(`https://api.spotify.com/v1/search`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            params: {
+                q: songName,
+                type: 'track'
+            }
+        });
+
+        console.log(response.data.tracks.items);
+        setResults(response.data.tracks.items);
+    };
+
+    return (
+        <div>
+            <form
+                className={style['form-container']}
+                onSubmit={checkForSong}>
+                <input name="song_name" type="text" />
+                <button>Search</button>
+            </form>
+        </div>
+    );
+};
+
+export default SearchEngine;
