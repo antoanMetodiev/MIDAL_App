73af02eb67308ba951587015dac3c724a7994d0e
Antoan Metodiev <Boksnem@abv.bg>
2024-09-27 15:50:58 +0300

    add PaymentPlans component (maybe will be deleted soon)

---
+++ client/src/components/DiscoverPage/structure/PaymentPlans/PaymentPlans.jsx
@@ -0,0 +1,19 @@
+
+import style from "./PaymentPlans.module.css";
+
+const PaymentPlans = () => {
+
+
+    return (
+        <article>
+            <h2>Press 'Play' on the perfect plan in MIDAL.</h2>
+
+            <div className={style['plans-container']}>
+
+            </div>
+
+        </article>
+    );
+};
+
+export default PaymentPlans;
