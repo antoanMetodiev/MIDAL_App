6f9c5ac466db388f8eb92673bfefb02fe8533952
Antoan Metodiev <Boksnem@abv.bg>
2024-10-02 06:17:05 +0300

    add config for Spotify

---
+++ client/src/components/GoogleAuth/spotifyClient_data.js
@@ -0,0 +1,8 @@
+// For Front-End:
+export const CLIENT_ID = '1079367636616-evi018qbk36u870mf9lbjvu0vhvv8sbg.apps.googleusercontent.com';
+export const REDIRECT_URI = 'http://localhost:5173/songs-and-podcasts';
+export const CLIENT_SECRET = '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';
+export const SCOPE = "https://www.googleapis.com/auth/youtube.readonly"; 
+
+
+
