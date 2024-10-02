95124ebc1e57ddf2ef2e3ee564d46dcfecdd0949
Antoan Metodiev <Boksnem@abv.bg>
2024-10-02 06:18:20 +0300

    orginize component

--- client/src/components/DiscoverPage/DiscoverPage.jsx
+++ client/src/components/DiscoverPage/DiscoverPage.jsx
@@ -3,12 +3,22 @@ import CarouselPlaylist from "./structure/CarouselPlaylist/CarouselPlaylist";
 import SoundsInfo from "./structure/SoundsInfo/SoundsInfo";
 import PaymentPlans from "./structure/PaymentPlans/PaymentPlans";
 
-const DiscoverPage = () => {
+const DiscoverPage = ({
+    setShowRegisterHandler,
+    setShowLoginHandler,
+    isRegistered,
+    setIsRegisteredHandler,
+}) => {
 
     
     return (
         <article>
-            <Overview />
+            <Overview 
+                isRegistered={isRegistered}
+                setIsRegisteredHandler={setIsRegisteredHandler}
+                setShowRegisterHandler={setShowRegisterHandler} 
+                setShowLoginHandler={setShowLoginHandler} 
+            />
             <CarouselPlaylist />
             <SoundsInfo />
             <PaymentPlans />
