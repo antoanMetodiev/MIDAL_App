737645bcfcf49bfd7895270d08320262dcda7802
Antoan Metodiev <Boksnem@abv.bg>
2024-10-02 06:22:06 +0300

    add config

---
+++ server/configs/spotifyConfig.js
@@ -0,0 +1,14 @@
+// For Server:
+const CLIENT_ID = '1079367636616-evi018qbk36u870mf9lbjvu0vhvv8sbg.apps.googleusercontent.com';
+const REDIRECT_URI = 'http://localhost:5173/songs-and-podcasts';
+const CLIENT_SECRET = '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';
+const SCOPE = "https://www.googleapis.com/auth/youtube.readonly"; 
+
+module.exports = {
+    CLIENT_ID,
+    REDIRECT_URI,
+    CLIENT_SECRET,
+    SCOPE,
+};
+
+
