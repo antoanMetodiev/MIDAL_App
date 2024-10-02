14e58ce84936ae1e5ba4704464e474338a89128b
Antoan Metodiev <Boksnem@abv.bg>
2024-10-02 06:23:03 +0300

    add song and googleAuth controllers in app middlewares

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,14 @@
 const app = require('express')();
 
 // Тук ще събирам контролери:
+const googleAuthController = require('./controllers/googleAuthController');
+const songController = require('../server/controllers/songController');
 const authController = require('./controllers/authController');
 
 // A тук midlewares:
+app.use(googleAuthController);
+app.use(songController);
 app.use(authController);
-
 
 module.exports = app;
 
