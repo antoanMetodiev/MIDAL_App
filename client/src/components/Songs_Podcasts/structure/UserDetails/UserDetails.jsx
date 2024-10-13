96ca5a1cffd80257edef9fd9dce8b272b5e6c95f
Antoan Metodiev <Boksnem@abv.bg>
2024-10-13 19:11:13 +0300

    add UserDetails where i implement inf about the user

---
+++ client/src/components/Songs_Podcasts/structure/UserDetails/UserDetails.jsx
@@ -0,0 +1,17 @@
+
+
+
+import style from "./UserDetails.module.css";
+
+const UserDetails = () => {
+
+
+
+    return (
+        <div className={style['user-details-container']}>
+
+        </div>
+    );
+}
+
+export default UserDetails;
