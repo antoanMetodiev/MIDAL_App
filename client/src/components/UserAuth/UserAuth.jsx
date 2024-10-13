ebf6496a0432a93c1665d1db8cfe5f8269e6ee45
Antoan Metodiev <Boksnem@abv.bg>
2024-10-13 19:15:22 +0300

    order code

--- client/src/components/UserAuth/UserAuth.jsx
+++ client/src/components/UserAuth/UserAuth.jsx
@@ -1,14 +1,34 @@
-
 import style from "./UserAuth.module.css";
 import Register from "./structure/Register/Register";
+import Login from "./structure/Login/Login";
+import { useState } from "react";
 
-const UserAuth = () => {
+const UserAuth = ({
+    showRegister,
+    showLogin,
+    setShowLoginHandler,
+    setShowRegisterHandler,
+    setUserDataHandler,
+}) => {
+    const [hideBoth, setHideBoth] = useState(false);
 
+    function setHideBothHandler() {
+        setShowLoginHandler(false);
+        setShowRegisterHandler(false);
+    };
 
     return (
-        <article className={style['register-login-wrapper']}>
-            <Register />
-        </article>
+        <>
+            {showRegister &&
+                <Register
+                    setUserDataHandler={setUserDataHandler}
+                    setHideBothHandler={setHideBothHandler}
+                />}
+            {showLogin && <Login
+                setUserDataHandler={setUserDataHandler}
+                setHideBothHandler={setHideBothHandler}
+            />}
+        </>
     );
 };
 
--- server/services/authService.js
+++ server/services/authService.js
@@ -1,30 +1,13 @@
-require('dotenv').config();
 const bcrypt = require('bcrypt');
-const cookie = require('cookie'); // Импортиране на cookie библиотеката
-const jwt = require('jsonwebtoken'); // Добавете библиотека за JWT
-
 const UserModel = require('../models/UserModel');
-
-const SECRET = 'NA NIKOI NE MU PUKA KAKUV TI E SIKRETA';
 
 async function registerUser(res, { user }) {
 
-
-
     try {
-        // debugger;
         const hashedPassword = await bcrypt.hash(user.password, 12);
         user.password = hashedPassword;
 
         const userData = await UserModel.create(user);
-
-        res.setHeader('Set-Cookie', userData, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production', // Само за HTTPS при продукция
-            maxAge: 60 * 60 * 48, // 2 дни
-            path: '/', // Път на cookies
-        });
-
         return userData;
 
     } catch (error) {
