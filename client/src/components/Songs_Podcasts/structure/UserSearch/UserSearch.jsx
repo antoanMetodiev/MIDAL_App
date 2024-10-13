e6634b69f94ddd143d71ad13a4bf4f85c98fc451
Antoan Metodiev <Boksnem@abv.bg>
2024-10-13 19:12:01 +0300

    add logics for search user

--- client/src/components/Songs_Podcasts/structure/UserSearch/UserSearch.jsx
+++ client/src/components/Songs_Podcasts/structure/UserSearch/UserSearch.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import style from "./UserSearch.module.css"
 
@@ -8,11 +8,34 @@ const UserSearch = ({
 }) => {
     const [receivedUsers, setReceivedUsers] = useState([]);
 
-    useEffect(() => {
+    // Получен FriendList и МОЯТ FriendList..
+    const [myFriendsList, setMyFriendsList] = useState([]);
+    const nameRef = useRef(null);
 
+
+    useEffect(() => {
+        const friendsList = JSON.parse(localStorage.getItem('MIDAL_USER')).friendsList;
+
+        debugger;
+        console.log(friendsList)
+
+        const currentUsers = [];
+        for (let i = 0; i < friendsList.length; i++) {
+            currentUsers.push(friendsList[i].name + "-" + friendsList[i].id);
+        }
+
+        setMyFriendsList(currentUsers);
+    }, []);
+
+    useEffect(() => {
         const solve = async () => {
             let searchedUsers = await axios.post("http://localhost:8080/search-user", { username: searchTerm });
-            console.log(searchedUsers.data);
+            const usersData = searchedUsers.data;
+
+            // const responsedUsers = [];
+            // for (let i = 0; i < searchedUsers.data.length; i++) {
+            //     responsedUsers.push(usersData[i].username + "-" + usersData[i]._id);
+            // }
 
             setReceivedUsers(searchedUsers.data);
         };
@@ -24,142 +47,68 @@ const UserSearch = ({
     }, [searchTerm]);
 
 
+    const addFriend = async (id, name, imgURL) => {
+        const myData = JSON.parse(localStorage.getItem('MIDAL_USER'));
+
+        debugger;
+        const message = await axios.post('http://localhost:8080/add-friend', { id: id, name: name, imgURL: imgURL, myData: myData });
+
+        // Добавяне и в localceStorage за да не налага да рефрешвам:
+        const newFriend = { id: id, name: name, imgURL: imgURL };
+        myData.friendsList.push(newFriend);
+        localStorage.setItem('MIDAL_USER', JSON.stringify(myData));
+
+        const newObject = myFriendsList.slice();
+        newObject.push(newFriend);
+        setMyFriendsList(newObject);
+        console.log(newObject);
+    };
+
+
+    console.log(myFriendsList);
+
+    const addedFriendOptions = () => {
+
+
+    };
+
+    console.log(myFriendsList);
+
     return (
         <div className={style['received-users-container']}>
-            
 
-            <div className={style['user-container']}>
-                <img 
-                    className={style['user-image']}
-                    src="https://stylecaster.com/wp-content/uploads/2024/02/Maria-Bachelor.jpg" 
-                    alt="mace"
-                 />
-                <h3>Sandra</h3>
-                <h6>Профил</h6>
-            </div>
+            {receivedUsers.length > 0 && (
+                receivedUsers.map(user => (
+                    <div className={style['user-container']} key={user._id}>
+                        <img
+                            className={style['user-image']}
+                            src={user.imageURL}
+                            alt="User Avatar"
+                        />
+                        <h3 ref={nameRef}>{user.username}</h3>
+                        <h6>Профил</h6>
+
+                        {myFriendsList.includes(`${user.username}-${user._id}`) ? (
+                            <h4
+                                className={style['add-friend']}
+                                onClick={() => {
+                                    addedFriendOptions(user.id);
+                                }}
+                            >Приятели</h4>
+                        ) : (
+
+                            <h4
+                                onClick={() => {
+                                    addFriend(user._id, user.username, user.imageURL);
+                                }}
+                                className={style['add-friend']}
+                            >Добави Приятел</h4>
+                        )}
 
 
-
-            <div className={style['user-container']}>
-                <img 
-                    className={style['user-image']}
-                    src="https://stylecaster.com/wp-content/uploads/2024/02/Maria-Bachelor.jpg" 
-                    alt="mace"
-                 />
-                <h3>Sandra</h3>
-                <h6>Профил</h6>
-            </div>
-            <div className={style['user-container']}>
-                <img 
-                    className={style['user-image']}
-                    src="https://stylecaster.com/wp-content/uploads/2024/02/Maria-Bachelor.jpg" 
-                    alt="mace"
-                 />
-                <h3>Sandra</h3>
-                <h6>Профил</h6>
-            </div>
-            <div className={style['user-container']}>
-                <img 
-                    className={style['user-image']}
-                    src="https://stylecaster.com/wp-content/uploads/2024/02/Maria-Bachelor.jpg" 
-                    alt="mace"
-                 />
-                <h3>Sandra</h3>
-                <h6>Профил</h6>
-            </div>
-            <div className={style['user-container']}>
-                <img 
-                    className={style['user-image']}
-                    src="https://stylecaster.com/wp-content/uploads/2024/02/Maria-Bachelor.jpg" 
-                    alt="mace"
-                 />
-                <h3>Sandra</h3>
-                <h6>Профил</h6>
-            </div>
-            <div className={style['user-container']}>
-                <img 
-                    className={style['user-image']}
-                    src="https://stylecaster.com/wp-content/uploads/2024/02/Maria-Bachelor.jpg" 
-                    alt="mace"
-                 />
-                <h3>Sandra</h3>
-                <h6>Профил</h6>
-            </div>
-            <div className={style['user-container']}>
-                <img 
-                    className={style['user-image']}
-                    src="https://stylecaster.com/wp-content/uploads/2024/02/Maria-Bachelor.jpg" 
-                    alt="mace"
-                 />
-                <h3>Sandra</h3>
-                <h6>Профил</h6>
-            </div>
-            <div className={style['user-container']}>
-                <img 
-                    className={style['user-image']}
-                    src="https://stylecaster.com/wp-content/uploads/2024/02/Maria-Bachelor.jpg" 
-                    alt="mace"
-                 />
-                <h3>Sandra</h3>
-                <h6>Профил</h6>
-            </div>
-            <div className={style['user-container']}>
-                <img 
-                    className={style['user-image']}
-                    src="https://stylecaster.com/wp-content/uploads/2024/02/Maria-Bachelor.jpg" 
-                    alt="mace"
-                 />
-                <h3>Sandra</h3>
-                <h6>Профил</h6>
-            </div>
-            <div className={style['user-container']}>
-                <img 
-                    className={style['user-image']}
-                    src="https://stylecaster.com/wp-content/uploads/2024/02/Maria-Bachelor.jpg" 
-                    alt="mace"
-                 />
-                <h3>Sandra</h3>
-                <h6>Профил</h6>
-            </div>
-            <div className={style['user-container']}>
-                <img 
-                    className={style['user-image']}
-                    src="https://stylecaster.com/wp-content/uploads/2024/02/Maria-Bachelor.jpg" 
-                    alt="mace"
-                 />
-                <h3>Sandra</h3>
-                <h6>Профил</h6>
-            </div>
-            <div className={style['user-container']}>
-                <img 
-                    className={style['user-image']}
-                    src="https://stylecaster.com/wp-content/uploads/2024/02/Maria-Bachelor.jpg" 
-                    alt="mace"
-                 />
-                <h3>Sandra</h3>
-                <h6>Профил</h6>
-            </div>
-            <div className={style['user-container']}>
-                <img 
-                    className={style['user-image']}
-                    src="https://stylecaster.com/wp-content/uploads/2024/02/Maria-Bachelor.jpg" 
-                    alt="mace"
-                 />
-                <h3>Sandra</h3>
-                <h6>Профил</h6>
-            </div>
-            <div className={style['user-container']}>
-                <img 
-                    className={style['user-image']}
-                    src="https://stylecaster.com/wp-content/uploads/2024/02/Maria-Bachelor.jpg" 
-                    alt="mace"
-                 />
-                <h3>Sandra</h3>
-                <h6>Профил</h6>
-            </div>
-
-            
-
+                    </div>
+                ))
+            )}
         </div>
     )
 }
