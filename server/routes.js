5532bd85d31f7b13b13f6bcbe314b9a85297a3dd
Antoan Metodiev <Boksnem@abv.bg>
2024-10-13 19:10:10 +0300

    add searchUserController, add_remove_friend_Controller  and  listeningFriendsController

--- server/routes.js
+++ server/routes.js
@@ -4,11 +4,17 @@ const app = require('express')();
 const googleAuthController = require('./controllers/googleAuthController');
 const songController = require('../server/controllers/songController');
 const authController = require('./controllers/authController');
+const searchUserController = require('./controllers/searchUserController');
+const add_remove_friend_Controller = require('./controllers/add_remove_friend_Controller');
+const listeningFriendsController = require('./controllers/listeningFriendsController');
 
 // A тук midlewares:
 app.use(googleAuthController);
 app.use(songController);
 app.use(authController);
+app.use(searchUserController);
+app.use(add_remove_friend_Controller);
+app.use(listeningFriendsController);
 
 module.exports = app;
 
