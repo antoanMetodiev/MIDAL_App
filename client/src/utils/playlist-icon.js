bcd5dd58fd9a5058e9d19b3aac02b9399fbb2caf
Antoan Metodiev <Boksnem@abv.bg>
2024-10-27 03:04:40 +0200

    create file where combine all images about playlists

---
+++ client/src/utils/playlist-icon.js
@@ -0,0 +1,19 @@
+import starIMAGE from "../components/Songs_Podcasts/resources/images/star.png";
+import podcastIMAGE from "../components/Songs_Podcasts/resources/images/podcasts.png";
+import boyIMAGE from "../components/Songs_Podcasts/resources/images/boy.png";
+import girlIMAGE from "../components/Songs_Podcasts/resources/images/girl.png";
+import likedSongsPlaylistIMAGE from "../components/Songs_Podcasts/resources/images/liked-songs-playlist.png";
+
+export const playlistIcons = {
+    starImage: starIMAGE,
+    podcastImage: podcastIMAGE,
+    boyImage: boyIMAGE,
+    girlImage: girlIMAGE,
+    likedSongsPlaylistImage: likedSongsPlaylistIMAGE,
+};
+
+{/* <img src={starImage} alt="starImage" />
+<img src={likedSongsPlaylistImage} alt="likedSongsPlaylistImage" />
+<img src={podcastImage} alt="podcastImage" />
+<img src={boyImage} alt="boyImage" />
+<img src={girlImage} alt="girlImage" /> */}
